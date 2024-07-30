3d5652876aeedb9375da00f79e67dc439abda282
Alena Lyalyushkina <pluto_16@mail.ru>
2024-07-30 15:35:18 +1000

    init commit

---
+++ app/contacts/page.tsx
@@ -0,0 +1,69 @@
+import { CustomSection } from '@/components/custom-section';
+import { HeroSectionStatic } from './hero-section-static';
+import { Map } from '@/components/main-page/map';
+import { SquareLinksSection } from '@/components/main-page/square-links-section';
+// import { heroData } from "@/components/main-page/hero-data"; // Предполагается, что heroData экспортируется из отдельного файла
+import { TypeLines } from '@/lib/definitions';
+import { TopRounder } from '@/components/top-rounder';
+import { ContactsInfo } from './contacts-info';
+import { BigRectLinkProps } from '@/components/main-page/big-rect-link';
+import { BigRectLink } from '@/components/main-page/big-rect-link';
+
+
+interface Image {
+  src: string;
+  srcSmall?: string;
+  alt?: string;
+}
+
+export interface SimpleHeroData {
+  title: string;
+  image: Image;
+}
+
+export const simpleHeroData: SimpleHeroData = {
+  title: 'Контакты',
+  image: {
+    src: '/images/image3.svg',
+    srcSmall: '/images/hero-bg-contacts-sm.svg',
+    alt: 'Контакты',
+  },
+};
+
+const bigRectLinkData: BigRectLinkProps = {
+  url: "/legal",
+  title: "Юридическим лицам",
+  start: {
+    mobile: "1",
+    tablet: "1",
+    desktop: "9",
+  },
+  end: {
+    mobile: "13",
+    tablet: "7",
+    desktop: "13",
+  },
+  image: {
+    src: "/images/icons/briefcase.svg",
+    alt: "Юридическим лицам",
+    width: 40,
+    height: 38,
+  },
+};
+
+export default function Contacts() {
+  return (
+    <>
+      <CustomSection>
+        <HeroSectionStatic heroData={simpleHeroData} className='' />
+        <TopRounder />
+      </CustomSection>
+      <Map />
+      <CustomSection>
+        <ContactsInfo />
+        <BigRectLink link={bigRectLinkData} />
+        <SquareLinksSection />
+      </CustomSection>
+    </>
+  );
+}
---
+++ components/text/text.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+type TypeTextProps = {
+  children: React.ReactNode;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function H1({ children, className }: TypeTextProps) {
+  return (
+    <h1
+      className={cn(
+        "text-white text-4xl sm:text-lg md:text-2xl font-bold sm:font-bold",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export function H3({ children, className }: TypeTextProps) {
+  return (
+    <h3
+      className={cn(
+        "text-foreground text-base sm:text-lg md:text-2xl font-semibold sm:font-bold",
+        className
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
+
+export function H4({ children, className }: TypeTextProps) {
+  return (
+    <h4
+      className={cn(
+        "text-foreground text-sm sm:text-xl md:text-2xl font-semibold sm:font-bold",
+        className
+      )}
+    >
+      {children}
+    </h4>
+  );
+}
+
+export function H5({ children, className }: TypeTextProps) {
+  return (
+    <h4
+      className={cn("text-foreground text-sm sm:text-lg font-bold", className)}
+    >
+      {children}
+    </h4>
+  );
+}
